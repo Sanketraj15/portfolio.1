1059fa7e4c85907b435f71091d469c944d8cf961
Sanketraj15 <141701014+Sanketraj15@users.noreply.github.com>
2025-09-12 14:12:12 +0530

    Fix: Remove base path for Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio.1/",
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
     alias: {
